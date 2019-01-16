9a54655acd2a766355bc1468017aedc8f82447d1
dvyurij <dvyurij@gmail.com>
2019-01-16 10:06:06 +0900

    [정준호]푸시

---
+++ ARCore Test Android/Assets/GoogleARCore/Examples/Common/Scripts/ButtonControlManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonControlManager : MonoBehaviour {
+
+	ButtonControlManager() {
+		Btntype = ButtonType.NULL;
+	}
+
+	public static ButtonType Btntype { get; set; }
+
+	// control button function by button text
+	public void OnClick()
+	{
+		Debug.Log(this.gameObject.GetComponentInChildren<Text>().text + " Clicked!");
+		switch (this.gameObject.GetComponentInChildren<Text>().text)
+		{
+			case "Swim":
+				Btntype = ButtonType.Swim;
+				break;
+			case "Stand":
+				Btntype = ButtonType.Stand;
+				break;
+			case "Jump":
+				Btntype = ButtonType.Jump;
+				break;
+			case "Walk":
+				Btntype = ButtonType.Walk;
+				break;
+			case "Attack":
+				Btntype = ButtonType.Attack;
+				break;
+			case "Remove":
+				Btntype = ButtonType.Remove;
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+
